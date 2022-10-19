a23536f1917d25d127aa95a66789a0b546fd9340
Chieh-Jen Wang <cjwang0318@gmail.com>
2022-10-19 16:42:20 +0800

    version 0.2

--- Rider_Ranking.py
+++ Rider_Ranking.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
-import tool_box as tb
+import pandas as pd
 
 
 def connet_DB():
@@ -32,6 +32,7 @@ def close_DB(connection, cursor):
     connection.close()
     print("資料庫連線已關閉")
 
+
 def query_DB(cursor, query):
     cursor.execute(query)
     # 取回全部的資料
@@ -40,13 +41,39 @@ def query_DB(cursor, query):
     return records
 
 
+def get_tesing_get_send_address(cursor, num_of_testing_instance):
+    search_query = "SELECT `GET_ADDRESS_2_sub`, `SEND_ADDRESS_2_sub` FROM `RMP_Get_Send_Count` LIMIT " + str(
+        num_of_testing_instance)
+    records = query_DB(cursor, search_query)
+    return records
+
+
+def ranker_1(row):
+    get_address = row["get_address"]
+    send_address = row["send_address"]
+    search_query = f"SELECT `GET_EMPLOYEE_CODE`, `count`, `min_time_diff` FROM `RMP_Rider_Get_Min_TimeDiff` WHERE `GET_ADDRESS_2_sub` LIKE '{get_address}' AND `SEND_ADDRESS_2_sub` LIKE '{send_address}' ORDER BY `count` DESC"
+    print(search_query)
+    # print("The class is %s" % row["get_address"])
+
+
 if __name__ == '__main__':
-    connection, cursor=connet_DB()
-    send_address="新竹市東區新莊街230號"
-    arrival_address="新竹縣寶山鄉創新一路13號"
-    search_query="SELECT `GET_EMPLOYEE_CODE`, `count`, `min_time_diff` FROM `RMP_Rider_Get_Min_TimeDiff` WHERE `GET_ADDRESS_2_sub` LIKE '新竹市東區新莊街230號' AND `SEND_ADDRESS_2_sub` LIKE '新竹縣寶山鄉創新一路13號' ORDER BY `count` DESC"
-    # 列出查詢的資料
-    records=query_DB(cursor, search_query)
-    for (employee_code, count, min_time_diff) in records:
-        print("employee_code: %s, count: %d, min_time_diff: %d" % (employee_code, count, min_time_diff))
+    connection, cursor = connet_DB()
+    send_address = "新竹市東區新莊街230號"
+    arrival_address = "新竹縣寶山鄉創新一路13號"
+    testing_address = []
+    # query top 100 get and send address for testing
+    records = get_tesing_get_send_address(cursor, 100)
+    for (get_address, send_address) in records:
+        # print("get_address: %s, send_address: %s" % (get_address, send_address))
+        testing_address.append([get_address, send_address])
+    df_testing_address = pd.DataFrame(testing_address)
+    df_testing_address.columns = ["get_address", "send_address"]
+    # print(df_testing_address)
+    df_testing_address.apply(ranker_1, axis=1)
+
+    # search_query="SELECT `GET_EMPLOYEE_CODE`, `count`, `min_time_diff` FROM `RMP_Rider_Get_Min_TimeDiff` WHERE `GET_ADDRESS_2_sub` LIKE '新竹市東區新莊街230號' AND `SEND_ADDRESS_2_sub` LIKE '新竹縣寶山鄉創新一路13號' ORDER BY `count` DESC"
+    # # 列出查詢的資料
+    # records=query_DB(cursor, search_query)
+    # for (employee_code, count, min_time_diff) in records:
+    #     print("employee_code: %s, count: %d, min_time_diff: %d" % (employee_code, count, min_time_diff))
     close_DB(connection, cursor)
